70555e1b8a32241f7e46f3b8f92c662184a239c9
tommasoviciani <49819683+tommasoviciani@users.noreply.github.com>
2021-08-18 21:19:27 +0200

    feat: implementa nuovo codice in m1911

--- Entities/Weapons/Guns/Pistols/M1911/M1911.cs
+++ Entities/Weapons/Guns/Pistols/M1911/M1911.cs
@@ -1,51 +1,46 @@
 using Godot;
 using System;
 
-public class M1911 : MeshInstance, IGun, IEquipable
+public class M1911 : EquipableGun
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        GD.Print("M1911");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
-    {
-        
+    {   
+        if (IsEquipped)
+        {
+            if (Input.IsActionPressed("shoot") && AmmoManager.HasAmmoInMagazine()) Shoot();
+            if (Input.IsActionPressed("reload") && !AmmoManager.IsMagazineFull()) Reload();
+        }
     }
 
-    public void Shoot()
+    public override void Reload()
     {
-        GD.Print("M1911 fire");
+        AmmoManager.ReloadMagazine();
+        GetNode<EventsBus>(Constants.NodePath.EventsBus).EmitSignal("GunAmmoChanged", AmmoManager);
     }
 
-    public void Reload()
+    public override void Shoot()
     {
-        GD.Print("M1911 reload");
+        GD.Print("M1911 shoot");
+        AmmoManager.Consume();
+        GetNode<EventsBus>(Constants.NodePath.EventsBus).EmitSignal("GunAmmoChanged", AmmoManager);
     }
 
-    public Boolean IsFiring() 
-    {
-        return false;
-    }
-
-    public Boolean IsReloading()
-    {
-        return false;
-    }
-
-    public void Equip()
+    public override void Equip()
     {
         GD.Print("M1911 equip");
+        IsEquipped = true;
+        GetNode<EventsBus>(Constants.NodePath.EventsBus).EmitSignal("GunEquipped", this);
     }
 
-    public void Unequip()
+    public override void Unequip()
     {
         GD.Print("M1911 unequip");
+        IsEquipped = false;
     }
 }
